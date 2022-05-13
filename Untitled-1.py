daa0d5cab6a9b4542b4ee5b058075185de9f2ed6
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-05-13 10:14:52 +0000

    up

--- Untitled-1.py
+++ Untitled-1.py
@@ -143,3 +143,125 @@ print("input_size =",input_size,"output_size =",output_size)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
 print("The model will be running on", device, "device\n") 
+
+
+# Instantiate the model 
+model = WDCNN(input_size, output_size) 
+model.to(device)
+# Function to save the model 
+def saveModel(): 
+    path = "./NetModel.pth" 
+    torch.save(model.state_dict(), path) 
+
+# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+
+# Training Function 
+def train(num_epochs): 
+    best_accuracy = 0.0 
+     
+    print("Begin training...") 
+    for epoch in range(1, num_epochs+1): 
+        running_train_loss = 0.0 
+        running_accuracy = 0.0 
+        running_vall_loss = 0.0 
+        total = 0 
+ 
+        # Training Loop 
+        for data in train_loader: 
+        #for data in enumerate(train_loader, 0): 
+            inputs, outputs = data  # get the input and real species as outputs; data is a list of [inputs, outputs] 
+            optimizer.zero_grad()   # zero the parameter gradients          
+            predicted_outputs = model(inputs)   # predict output from the model 
+            train_loss = loss_fn(predicted_outputs, outputs)   # calculate loss for the predicted output  
+            train_loss.backward()   # backpropagate the loss 
+            optimizer.step()        # adjust parameters based on the calculated gradients 
+            running_train_loss +=train_loss.item()  # track the loss value 
+ 
+        # Calculate training loss value 
+        train_loss_value = running_train_loss/len(train_loader) 
+ 
+        # Validation Loop 
+        with torch.no_grad(): 
+            model.eval() 
+            for data in validate_loader: 
+               inputs, outputs = data 
+               predicted_outputs = model(inputs) 
+               val_loss = loss_fn(predicted_outputs, outputs) 
+             
+               # The label with the highest value will be our prediction 
+               _, predicted = torch.max(predicted_outputs, 1) 
+               running_vall_loss += val_loss.item()  
+               total += outputs.size(0) 
+               running_accuracy += (predicted == outputs).sum().item() 
+ 
+        # Calculate validation loss value 
+        val_loss_value = running_vall_loss/len(validate_loader) 
+                
+        # Calculate accuracy as the number of correct predictions in the validation batch divided by the total number of predictions done.  
+        accuracy = (100 * running_accuracy / total)     
+ 
+        # Save the model if the accuracy is the best 
+        if accuracy > best_accuracy: 
+            saveModel() 
+            best_accuracy = accuracy 
+         
+        # Print the statistics of the epoch 
+        print('Completed training batch', epoch, 'Training Loss is: %.4f' %train_loss_value, 'Validation Loss is: %.4f' %val_loss_value, 'Accuracy is %d %%' % (accuracy))
+
+# Function to test the model 
+def test(): 
+    # Load the model that we saved at the end of the training loop 
+    model = Network(input_size, output_size) 
+    path = "NetModel.pth" 
+    model.load_state_dict(torch.load(path)) 
+     
+    running_accuracy = 0 
+    total = 0 
+ 
+    with torch.no_grad(): 
+        for data in test_loader: 
+            inputs, outputs = data 
+            outputs = outputs.to(torch.float32) 
+            predicted_outputs = model(inputs) 
+            _, predicted = torch.max(predicted_outputs, 1) 
+            total += outputs.size(0) 
+            running_accuracy += (predicted == outputs).sum().item() 
+ 
+        print('Accuracy of the model based on the test set of', test_split ,'inputs is: %d %%' % (100 * running_accuracy / total))    
+ 
+ 
+# Optional: Function to test which species were easier to predict  
+def test_species(): 
+    # Load the model that we saved at the end of the training loop 
+    model = Network(input_size, output_size) 
+    path = "NetModel.pth" 
+    model.load_state_dict(torch.load(path)) 
+     
+    labels_length = len(labels) # how many labels of Irises we have. = 3 in our database. 
+    labels_correct = list(0. for i in range(labels_length)) # list to calculate correct labels [how many correct setosa, how many correct versicolor, how many correct virginica] 
+    labels_total = list(0. for i in range(labels_length))   # list to keep the total # of labels per type [total setosa, total versicolor, total virginica] 
+  
+    with torch.no_grad(): 
+        for data in test_loader: 
+            inputs, outputs = data 
+            predicted_outputs = model(inputs) 
+            _, predicted = torch.max(predicted_outputs, 1) 
+             
+            label_correct_running = (predicted == outputs).squeeze() 
+            label = outputs[0] 
+            if label_correct_running.item():  
+                labels_correct[label] += 1 
+            labels_total[label] += 1  
+  
+    label_list = list(labels.keys()) 
+    for i in range(output_size): 
+        print('Accuracy to predict %5s : %2d %%' % (label_list[i], 100 * labels_correct[i] / labels_total[i])) 
+
+if __name__ == "__main__": 
+    num_epochs = 10
+    train(num_epochs) 
+    print('Finished Training\n') 
+    test() 
+    test_species() 
---
+++ ver2/dataloader.py
@@ -0,0 +1,111 @@
+# -*- coding:utf-8 -*-
+# python2
+
+'''
+functions:
+- load data and label from txt files
+- split train and test sets
+- resample
+- fast fourier transfrom
+- short time fourier transform
+'''
+
+import time
+import numpy as np
+import torch
+import torch.utils.data as data_utils
+import scipy.signal as sig
+
+def load_data(*data_file):
+    '''
+    data_file is a tuple with 1 or 2 elements;
+    first is vibration matrix,
+    second can be rotating speed matrix.
+    input two files only for rsn,
+    load_data twice if you have two vibration signal files.
+    rsn is a preprocessing for signal, 
+    it requires rotating speed matrix.
+    see paper "Convolutional Neural Networks for Fault Diagnosis
+    Using Rotating Speed Normalized Vibration".
+    '''
+    since = time.time()
+    data_arr = np.loadtxt(data_file[0])
+    if len(data_file)>1:
+        print('rsn used (see the paper).')
+        rpm_arr = np.loadtxt(data_file[1])
+        mean_rpm = np.mean(rpm_arr)
+        data_arr = np.power(mean_rpm,2)*data_arr / (rpm_arr*rpm_arr)
+    time_elapsed = time.time() - since
+    print('Data in file {} loaded in {:.0f}m {:.0f}s'.format(data_file, time_elapsed//60, time_elapsed%60))
+    return np.expand_dims(data_arr, axis=1)
+
+def load_label(label_file):
+    '''
+    load labels corrsponding to data
+    '''
+    return np.loadtxt(label_file, ndmin=1)
+
+def split_set(data, label, p=0.8):
+    '''
+    split data and label array into train and test partitions
+    '''
+    n_total = np.shape(data)[0]
+    n_train = int(n_total*p)
+    n_test = n_total - n_train
+    idx = np.random.permutation(n_total)
+    train_mask = idx[:n_train]
+    test_mask = idx[n_total-n_test:]
+    trainset = arr_to_dataset(data[train_mask], label[train_mask])
+    testset = arr_to_dataset(data[test_mask], label[test_mask])
+    return trainset, testset
+
+def arr_to_dataset(data_arr, label_vec):
+    '''
+    convert numpy array into tensor dataset
+    dataset = (X,y)
+    '''
+    data_ten = torch.from_numpy(data_arr).float()
+    label_ten = torch.from_numpy(label_vec).long()
+    dataset = data_utils.TensorDataset(data_ten, label_ten)
+    return dataset
+
+def resample_arr(data_arr, num=2048, method='Fourier'):
+    '''
+    Resample input numpy array into legnthed at num
+    methods option: 'Fourier' and 'Poly'
+    '''
+    if(method=='Fourier'):
+        new_arr = sig.resample(data_arr, num, axis=2)
+    elif(method=='Poly'):
+        new_arr = sig.resample_poly(data_arr, num, data_arr.shape[2], axis=2) 
+    return new_arr
+
+def fft_arr(arr):
+    '''
+    Fourier transform for signals in a Numpy array
+    '''
+    (n, _, l) = arr.shape
+    amp = np.zeros((n,1,l/2))
+    ang = np.zeros((n,1,l/2))
+    for idx in range(n):
+        ft = np.fft.fft(arr[idx,:,:])[:,:l/2]
+        amp[idx] = np.absolute(ft)
+        ang[idx] = np.angle(ft)
+    return amp, ang
+
+def stft_arr(arr, output_size=(32,32)):
+    '''
+    Short Time Fourier Transform for signals in a Numpy array
+    '''
+    (n, _, l) = arr.shape
+    spectrogram = np.zeros((n, 1, output_size[0], output_size[1]))
+    for idx in range(n):
+        f, t, S = sig.spectrogram(arr[idx,0,:], fs=10240, nperseg=output_size[0]*2, noverlap=0)
+        spectrogram[idx, 0] = np.absolute(S[:(output_size[0]), :(output_size[1])])
+    return spectrogram
+
+'''
+arr = np.random.rand(10, 1, 2048)
+arr = resample_arr(arr, 240, method='Poly')
+print(arr.shape)
+'''
